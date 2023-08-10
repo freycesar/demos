88d83abf1cb1fa588aececcc267e6f96260c76f6
freycesar <sunqingyu1997@gmail.com>
2023-08-10 16:49:32 +0900

    commit

--- Fixed_finger.py
+++ Fixed_finger.py
@@ -5,11 +5,12 @@ LastEditors: SUN Qingyu sunqingyu1997@gmail.com
 LastEditTime: 2023-05-25 16:21:47
 FilePath: \demos\Fixed_finger.py
 '''
-from pyautd3.link import SOEM
+from pyautd3.link import SOEM, Simulator, OnLostFunc
 from pyautd3.gain import Focus
-from pyautd3 import Controller, SilencerConfig, Clear, Synchronize, Stop, DEVICE_WIDTH, DEVICE_HEIGHT
+from pyautd3 import Controller, Geometry, SilencerConfig, Clear, Synchronize, Stop, DEVICE_WIDTH, DEVICE_HEIGHT
 from pyautd3.modulation import Static, Sine
 import numpy as np
+import math
 import time
 from pyautd3.stm import GainSTM
 import ctypes
@@ -18,19 +19,22 @@ import os
 
 # use cpp to get high precision sleep time
 dll = ctypes.cdll.LoadLibrary
-libc = dll(os.path.dirname(__file__) + '/../cpp/' + platform.system().lower() + '/HighPrecisionTimer.so') 
+libc = dll(os.path.dirname(__file__) + '/cpp/' + platform.system().lower() + '/HighPrecisionTimer.so') 
 
 W = 640
 H = 480
+
+def on_lost(msg: ctypes.c_char_p):
+        print(msg.decode('utf-8'), end="")
+        os._exit(-1)
 
 def stm_gain(autd: Controller):
     config = SilencerConfig.none()
     autd.send(config)
     m = Static(1.0)
-    stm = GainSTM(autd)
+    stm = GainSTM()
     radius = 1.0
     # radius_velocity = r_v
-    stm.frequency = 5
     size = 200
     n_updatecircle = 1
     # step = 0.2
@@ -43,6 +47,7 @@ def stm_gain(autd: Controller):
             p = radius * np.array([np.cos(theta), np.sin(theta), 0])
             f = Focus(center + p)
             stm.add(f)
+        stm.frequency = 5
         autd.send(m,stm)
         time.sleep(n_updatecircle * 1 / stm.frequency)
         radius += 0.01
@@ -67,27 +72,20 @@ def run(autd: Controller):
 
 
 if __name__ == '__main__':
-    autd = Controller()
 
- # Multiple AUTD
-    # The arrangement of the AUTDs:
-    # 1 → 2
-    #     ↓
-    # 4 ← 3
-    # (See from the upside)
-    autd.geometry.add_device([-DEVICE_WIDTH / 2, DEVICE_HEIGHT / 2, 0.], [0., 0., 0.])
-    autd.geometry.add_device([DEVICE_WIDTH / 2, DEVICE_HEIGHT / 2, 0.], [0., 0., 0.])
-    autd.geometry.add_device([DEVICE_WIDTH / 2, -DEVICE_HEIGHT / 2, 0.], [0., 0., 0.])
-    autd.geometry.add_device([-DEVICE_WIDTH / 2, -DEVICE_HEIGHT / 2, 0.], [0., 0., 0.])
-
-    if_use_simulator = input('If use simulator? [y: simulator] or [n: AUTD]: ')
-
-
-    link = SOEM().high_precision(True).build()
-
-    if not autd.open(link):
-        print('Failed to open Controller')
-        exit()
+    W_cos = math.cos(math.pi/12) * DEVICE_WIDTH
+    geometry = Geometry.Builder()\
+            .add_device([W_cos - (DEVICE_WIDTH - W_cos), DEVICE_HEIGHT - 10 + 12.5, 0.], [math.pi, math.pi/12, 0.])\
+            .add_device([W_cos - (DEVICE_WIDTH - W_cos), -10 - 12.5, 0.], [math.pi, math.pi/12, 0.])\
+            .add_device([-W_cos + (DEVICE_WIDTH - W_cos),  12.5, 0.], [0., math.pi/12, 0.])\
+            .add_device([-W_cos + (DEVICE_WIDTH - W_cos), -DEVICE_HEIGHT - 12.5, 0.], [0., math.pi/12, 0.])\
+            .build()
+    
+    # on_lost_func = OnLostFunc(on_lost)
+    link = Simulator().build()
+    # link = SOEM().on_lost(on_lost_func).build()    
+    autd = Controller.open(geometry, link)
+    # if_use_simulator = input('If use simulator? [y: simulator] or [n: AUTD]: ')
 
     autd.check_trials = 50
 
